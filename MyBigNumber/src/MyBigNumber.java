488c7d9e1fa089bd258b2be4d8bb616360e4fdaa
DatNguyen51600012 <43365714+DatNguyen51600012@users.noreply.github.com>
2018-12-22 20:24:05 +0700

    version v2.0

--- MyBigNumber/src/MyBigNumber.java
+++ MyBigNumber/src/MyBigNumber.java
@@ -1,13 +1,14 @@
 package javaapplication;
 
-import javaapplication.IReceiver;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javaapplication.IReceiver;
+
 
 /**
  * Tác giả:  Nguyễn Thành Đạt.
  * Class MyBigNumber để cộng 2 số.
- * Version v2.0
+ * Version v3.0
  * Date Modified 22/12/2018
  */
 
@@ -30,21 +31,26 @@ public class MyBigNumber {
     public String sum(final String str1, final String str2) {
         String num1 = str1;// chuỗi 1
         String num2 = str2;// chuỗi 2
-        String step = "";
-        String process = "";
-        String sum = "";//tao ra biến lưu kết quả
-        Pattern pattern = Pattern.compile("\\D"); // Chuỗi đại diện cho kí tự số từ [0-9]
-        final Matcher isError1 = pattern.matcher(num1);// biến để lưu dữ kết quả xét chuỗi s1 
-        final Matcher isError2 = pattern.matcher(num2);;// biến để lưu dữ kết quả xét chuỗi s2
         
+        String buoc = "";// số bước
+        String tong = "";// tạo ra biến lưu kết quả
+        
+       
+        int l1 = str1.length(); // biến độ dài chuỗi 1
+        int l2 = str2.length(); // biến độ dài chuỗi 2
+        final int maxLength = (l1 > l2) ? l1 : l2; // tìm max độ dài chuỗi 
         
         int nho = 0;// biến nhớ 
         int numSum = 0; // biến dùng để lưu kết phép cộng của từng kì tự trong chuỗi
         
-        int vtri ; // Vị trí kí tự trong chuỗi
+        int vtri ; // Vị trí các kí tự trong chuỗi
         
-        char char1 = '0';
-        char char2 = '0';
+        char c1 = '0';
+        char c2 = '0';
+        
+        Pattern pattern = Pattern.compile("\\D"); // Chuỗi đại diện cho kí tự số từ [0-9]
+        final Matcher isError1 = pattern.matcher(num1);// biến để lưu dữ kết quả xét chuỗi s1 
+        final Matcher isError2 = pattern.matcher(num2);;// biến để lưu dữ kết quả xét chuỗi s2
 
         // Bắt lỗi dữ liệu nhập vào nếu có
         // Nếu chuỗi chưa nhập vào thì tính là 0 ( trường hợp Null )
@@ -56,90 +62,87 @@ public class MyBigNumber {
             num2 = "0";        
         }
 
-        // Kiểm tra số âm
+        // Nếu số nhập vào là âm thì không tính và báo lỗi
         if (num1.charAt(0) == '-') {
             this.ireceiver.send("Không được nhập số âm : " + num1);
-            throw new NumberFormatException("Vui lòng không nhập số âm : " + num1);
+            throw new NumberFormatException("Bạn vui lòng không nhập số âm : " + num1);
         }
 
         if (num2.charAt(0) == '-') {
             ireceiver.send("Không được nhập số âm : " + num2);
-            throw new NumberFormatException("Vui lòng không nhập số âm : " + num2);
+            throw new NumberFormatException("Bạn vui lòng không nhập số âm : " + num2);
         }
 
-        // Kiểm tra kí tự đặc biệt
+        // Nếu nhập vào kí tự đặc biệt thì không tính và báo lỗi
         if (isError1.find()) {
             vtri = isError1.start() + 1;
-            this.ireceiver.send("Vị trí " + vtri + " trong chuổi " + num1 + " không phải số");
+            this.ireceiver.send("Vị trí " + vtri + " trong chuỗi " + num1 + " không phải số");
             throw new NumberFormatException(vtri + "");
         }
 
         if (isError2.find()) {
             vtri = isError2.start() + 1;
-            this.ireceiver.send("Vị trí " + vtri + " trong chuổi " + num2 + " không phải số");
+            this.ireceiver.send("Vị trí " + vtri + " trong chuỗi " + num2 + " không phải số");
             throw new NumberFormatException(vtri + "");
 
         }
-
-        //So sánh chuỗi
-        if (num1.length() < num2.length()) 
-        {
-
-            // đảo chuỗi
-            String item = num2;
-            num2 = num1;
-            num1 = item;
-        }
         
-        int i = 1;//biến đếm
+        int i = 1;// khởi tạo biến đếm trong vòng lặp
+        String p = "";
         
-        for (i = 1; i <= num1.length(); i++) { // Vòng lặp chạy từng kí tự chuỗi
-            char1 = num1.charAt(num1.length() - i);
+        for (i = 1; i <= maxLength ; i++) { // Vòng lặp xét từng kí tự trong hai chuỗi
+          
+            c1 = ((l1 - i) >= 0) ? num1.charAt(l1 - i) : '0';// nếu chuổi 1 hết ta sẽ ghi 0 ngược lại lấy kí tự cuối
+            c2 = ((l2 - i) >= 0) ? num2.charAt(l2 - i) : '0';//nếu chuổi 2 hết ta sẽ ghi 0 ngược lại lấy kí tự cuối
+            
+            numSum = (c1 - '0') + (c2 - '0') + nho;// cộng kí tự cuối của chuổi và phần nhớ(nếu có) vào với nhau
+            tong = Integer.toString(numSum % 10) + tong; //ghi kết quả cộng vào biến kết quả
 
-            //kiểm tra xem đã xét hêt chuỗi thứ 2 đã hết chưa
-            if (num2.length() - i >= 0) {
-                char2 = num2.charAt(num2.length() - i);
-                numSum = char1 - '0' + char2 - '0' + nho; //cộng từng kí tự của hai chuỗi và phần nhớ nếu có
-            } else {
-                numSum = char1 - '0' + nho; //cộng kí tự chuổi 1 và phần nhớ ki đã xét hết kí tự trong chuỗi 2
-            }
-            sum = Integer.toString(numSum % 10) + sum; //ghi kết quả cộng vào biến kết quả
+            nho = numSum / 10; // lưu vào biến nhớ là 1 nếu hai số cộng lại quá 10
 
-            //lưu phần nhớ là 1 nếu kết quả cộng lớn hơn 10 
-            nho = numSum / 10;
-
-            // kiểm tra xem có phần nhớ hay không
-            if (nho == 1) {
+            // xử lý các trường hợp của biến nhớ 
+            if (nho == 1) { 
                 if (num2.length() - i >= 0) {
-                    process = "\n" + "* Bước " + i + ", lấy " + char1 + " cộng " + char2 + " cộng " + nho + " được "
-                        + numSum + ", ghi " + numSum % 10 + ", nhớ " + nho + "\n";
+                    p = "* Bước " + i 
+                            + ", lấy " + c1 
+                            + " cộng " + c2 
+                            + " cộng " + nho 
+                            + " được " + numSum 
+                            + ", ghi " + numSum % 10 
+                            + ", nhớ " + nho 
+                            + "\n";
                 } else {
-                    process = "\n" + "* Bước " + i + ", lấy " + char1 + " cộng " + nho + " được " 
-                        + numSum + ", ghi " + numSum % 10 + ", nhớ " + nho + "\n";
+                    p = "* Bước " + i 
+                            + ", lấy " + c1 
+                            + " cộng " + nho 
+                            + " được " + numSum 
+                            + ", ghi " + numSum % 10 
+                            + ", nhớ " + nho 
+                            + "\n";
                 }
-            } else {
+            } else { 
                 if (num2.length() - i >= 0) {
-                    process =  "\n" + "* Bước " + i + ", lấy " + char1 + " cộng " + char2 + " được " 
+                    p =  "\n" + "* Bước " + i + ", lấy " + c1 + " cộng " + c2 + " được " 
                         + numSum + ", ghi " + numSum % 10 + "\n";
                 } else {
-                    process = "\n" + "* Bước " + i + ", lấy " + char1 + " cộng 0" + " được " 
+                    p = "\n" + "* Bước " + i + ", lấy " + c1 + " cộng 0" + " được " 
                         + numSum + ", ghi " + numSum % 10 + "\n";
                 }
             }
 
-            step = step + process;
+            buoc = buoc + p;
         }
 
         // ghi phần nhớ vào nếu phép cộng vượt số
         
         if (nho == 1) { 
-            sum = Integer.toString(nho) + sum;
-            step = step + "\n" + "* Bước " + i + ", lấy " + nho + " ghi trước kết quả" + "\n" ;
+            tong = Integer.toString(nho) + tong;
+            buoc = buoc + "\n" + "* Bước " + i + ", lấy " + nho + " ghi trước kết quả" + "\n" ;
         }
 
-        this.ireceiver.send(step);// gửi các bước đến ireceiver để in ra màn hình
+        this.ireceiver.send(buoc);// gửi các bước đến ireceiver để in ra màn hình
         
-        return sum;// trả về kết quả của phép cộng
+        return tong;// trả về kết quả của phép cộng
         
     }
 }
