ae7bdf09eb0af26e459a5a064c612d64f504ca7a
DatNguyen51600012 <43365714+DatNguyen51600012@users.noreply.github.com>
2018-12-22 20:25:03 +0700

    Add files via upload

--- MyBigNumber/test/MyBigNumberTest.java
+++ MyBigNumber/test/MyBigNumberTest.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package test;
+package Test;
 
-import addtwonum.IReceiver;
-import addtwonum.MyBigNumber;
+import javaapplication.IReceiver;
+import javaapplication.MyBigNumber;
 
 /**
  *
@@ -48,10 +48,10 @@ public class MyBigNumberTest implements IReceiver {
             MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 1:");
-            mybignumber.sum("9", "99");
+            System.out.println("PASS : " + mybignumber.sum("9", "99") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
     
@@ -60,22 +60,22 @@ public class MyBigNumberTest implements IReceiver {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 2:");
-            System.out.println("PASS : " + mybignumber.sum("99", "9"));
+            System.out.println("PASS : " + mybignumber.sum("99", "9") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
 
     public static void Test3() {
         try {
-             MyBigNumberTest test = new MyBigNumberTest();
+            MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 3:");
-            System.out.println("PASS : " + mybignumber.sum("99", "90"));
+            System.out.println("PASS : " + mybignumber.sum("99", "90") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
 
@@ -84,10 +84,10 @@ public class MyBigNumberTest implements IReceiver {
             MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 4:");
-            System.out.println(mybignumber.sum("999", "99"));
+            System.out.println(mybignumber.sum("999", "99") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
 
@@ -96,113 +96,118 @@ public class MyBigNumberTest implements IReceiver {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 5:");
-            System.out.println(mybignumber.sum("A", "999"));
+            System.out.println(mybignumber.sum("A", "999") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e) ;
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
-
+    
     public static void Test6() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 7:");
-            System.out.println(mybignumber.sum("8978", "9458*"));
+            System.out.println(mybignumber.sum("8978", "9458*") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
-    
+    // Trường hợp số thứ nhất chứa kí tự đặc biệt
     public static void Test7() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 7:");
-            System.out.println(mybignumber.sum("8978/", "9458*"));
+            System.out.println(mybignumber.sum("8978/", "9458*") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
-    
+    // Trường hợp số thứ nhất âm
     public static void Test8() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 8:");
-            System.out.println(mybignumber.sum("-54", "98"));
+            System.out.println(mybignumber.sum("-54", "98") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
-
+    // Trường hợp Null hai số nhập vào
     public static void Test9() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 9:");
-            System.out.println(mybignumber.sum("", ""));
+            System.out.println(mybignumber.sum("0", "") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
+    // Trường hợp có chữ trong số thứ nhất 
     public static void Test10() {
         try {
             MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 10:");
-            System.out.println(mybignumber.sum("98g4", "99"));
+            System.out.println(mybignumber.sum("98g4", "99") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
+    // Trường hợp có chữ trong số thứ 2
     public static void Test11() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 11:");
-            System.out.println(mybignumber.sum("9887", "897a"));
+            System.out.println(mybignumber.sum("9887", "897a") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
+    // TRường hợp Hai số có chứa chữ
     public static void Test12() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 12:");
-            System.out.println(mybignumber.sum("897a54", "8798a546"));
+            System.out.println(mybignumber.sum("897a54", "8798a546") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
+    //TRường hợp Null và số 
     public static void Test13() {
         try {
             MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 13:");
-            System.out.println("PASS : " + mybignumber.sum("", "546"));
+            System.out.println("PASS : " + mybignumber.sum("", "546") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
+    // Trường hợp Null và 0
     public static void Test14() {
         try {
              MyBigNumberTest test = new MyBigNumberTest();
             MyBigNumber mybignumber = new MyBigNumber(test);
             System.out.println("Test 14:");
-            System.out.println("PASS : " + mybignumber.sum("545", ""));
+            System.out.println("PASS : " + mybignumber.sum("", "0") + "\n");
         } catch (NumberFormatException e) {
             System.out.println(e);
-            System.out.println("FAIL");
+            System.out.println("FAIL\n");
         }
     }
 }
